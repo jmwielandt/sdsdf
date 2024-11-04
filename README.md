7ffe8ebe8b47ac51b200afc0aff7d2efec48437f
jmwielandt <git@stackblitz.com>
2024-11-04 16:07:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sdsdf
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jmwielandt/sdsdf)
---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+// ⚠️ Important! Please make sure the dependencies are up to date.
+// On code sandbox, you can refresh them in the Dependencies section (left-bottom)
+// On stackblitz, you can open the package.json file, update the versions,
+// then run npm install in the stackblitz terminal
+
+import Markdown from 'react-markdown';
+
+const markdown = `
+<a href="http://www.example.com" target="_blank">www.example.com</a>
+
+# Pluto
+
+**Pluto** (minor-planet designation: *134340 Pluto*)
+is a
+[dwarf planet](https://en.wikipedia.org/wiki/Dwarf_planet)
+in the
+[Kuiper belt](https://en.wikipedia.org/wiki/Kuiper_belt).
+`;
+
+function App() {
+  return <Markdown>{markdown}</Markdown>;
+}
+
+export default App;

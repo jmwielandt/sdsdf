3b51302ff79d4f7ace87ca102669018377170531
jmwielandt <jmgalleguillos@outlook.com>
2024-11-05 11:49:44 -0300

    add custom rehypePlugin to set target=_blank on all anchors

--- src/app.tsx
+++ src/app.tsx
@@ -6,10 +6,15 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import { visit } from 'unist-util-visit';
+import type { Root } from 'hast';
 
 const markdown = `
 <a href="http://www.example.com" target="_blank">www.example.com</a>
 
+www.example.com
+
+[lonk](http://www.example.com)
 
 I need to put a table too...
 |a|b|
@@ -19,10 +24,21 @@ I need to put a table too...
 
 const mdRawLink = `www.example.com`;
 
+function rehypeChangeTargetForIframeLinks() {
+  return (tree: Root) => {
+    visit(tree, "element", (node) => {
+      if (node.tagName === 'a') {
+        node.properties.target = "_blank"
+      }
+    })
+  }
+}
+
+
 function App() {
   return <>
   <h1>App 1</h1>
-  <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+  <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, rehypeChangeTargetForIframeLinks]}>
     {markdown}
   </Markdown>
   <h1>App 2</h1>
